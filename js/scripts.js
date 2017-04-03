438b0b88e4d1a30079a231e4e0be8998e337c621
Bartosz Czarnota <bartek.bootcamp@gmail.com>
2017-04-03 22:23:39 +0200

    First commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 // plik scripts.js
 
-var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
-	animal = ('Zielone słonie').toUpperCase();
-	//elephantsUpperCased = animal.toUpperCase();
-	animalReplace = text.replace('Papugi', animal)
-	//halfSentence = animalReplace.slice(0, 45);
+function rysujChoinke(x) {
+	
+	for (var y = 0 ; y < x ; y++) {
+			var star = '*';
+		
+		for (var z = 0 ; z < y*2-1 ; z++) {
+				var star = star + '*';
+		}
+		console.log(star)
+	}		
+}
 
-console.log(text);
-//console.log(halfSentence);
-console.log(animalReplace.substr(0, animalReplace.length/2));
+rysujChoinke(5);
